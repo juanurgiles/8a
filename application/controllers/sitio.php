14c49b198369f78c37e09e48454e2b7404221613
Angela <Angela@Juank>
2014-04-09 10:28:44 -0500

    Configuracion inicial

---
+++ application/controllers/sitio.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of sitio
+ *
+ * @author Angela
+ */
+class sitio extends CI_Controller {
+    //put your code here
+    public function __construct() {
+        parent::__construct();
+        $this->load->helper('url');
+    }
+    public function index(){
+        $this->load->view('index');
+    }
+}
